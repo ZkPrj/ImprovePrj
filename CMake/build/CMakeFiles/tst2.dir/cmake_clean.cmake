b85efad359e7bce97b8adc746ee51f398b24f3d7
zk_sima <zk_sima@163.com>
2020-06-11 23:49:56 +0800

    调整目录结构

---
+++ CMake/build/CMakeFiles/tst2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tst2.dir/main.cpp.obj"
+  "tst2.pdb"
+  "tst2.exe"
+  "tst2.exe.manifest"
+  "libtst2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tst2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/primer/build/chp2/CMakeFiles/PrimerChap2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrimerChap2.dir/src/chap2.obj"
+  "PrimerChap2.pdb"
+  "PrimerChap2.exe"
+  "PrimerChap2.exe.manifest"
+  "libPrimerChap2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrimerChap2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
